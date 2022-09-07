ecc25fcf17275db193c64f8d765ea8a075b71a7f
JeeWaa <jeewa228@gmail.com>
2022-09-07 23:14:43 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`app starting => ${port}`);
+})
+
+app.get('/', (req,res) => {
+    console.log('get request')
+    res.send('Hello')
+})
