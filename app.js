5cc6245238d7e66b4370c9f03915a4e8bbfab0af
JeeWaa <jeewa228@gmail.com>
2022-09-08 03:20:22 +0530

    connect mysql

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mysql = require('mysql')
+const db = require('../myapp/configs/db.configs')
 
 const app = express()
 const port = 4000
 
 app.use(express.json())
+
+const connection = mysql.createConnection(db.database)
+connection.connect(function (error) {
+    if (error) {
+        console.log(error)
+    }else {
+        console.log("Connected MYSQL")
+    }
+})
 
 app.listen(port, () => {
     console.log(`app starting => ${port}`);
---
+++ configs/db.configs.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'jeewa',
+        password: 'jeewa288$fernando',
+        database: 'express'
+    }
+}
